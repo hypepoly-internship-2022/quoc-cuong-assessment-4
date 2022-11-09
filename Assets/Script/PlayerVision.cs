3b3bec54169f3a4c79d15a66522b84af121d3772
chimpuchim <96745654+chimpuchim@users.noreply.github.com>
2022-11-09 17:13:03 +0700

    +add move vision player

--- Assets/Script/PlayerVision.cs
+++ Assets/Script/PlayerVision.cs
@@ -7,10 +7,9 @@ public class PlayerVision : MonoBehaviour
 {
 
     public float speedH = 2f;
-    public float speedV = 2f;
-
+    // public float speedV = 2f;
     private float yaw = 0.0f;
-    private float pitch = 0.0f;
+    // private float pitch = 0.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -21,20 +20,22 @@ public class PlayerVision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if(Mathf.Abs(yaw) < 40){
-            yaw += speedH * Input.GetAxis("Mouse X");
-            pitch -= speedV * Input.GetAxis("Mouse Y");
-        }
-        else{
-            if(yaw < 0){
-                yaw = -39.99999f;
-            } else {
-                yaw = 39.99999f;
+        if(Input.GetMouseButton(0)){
+            if(Mathf.Abs(yaw) < 40){
+                yaw += speedH * Input.GetAxis("Mouse X");
+
+                Debug.Log($"yaw: " + yaw);
+                // pitch -= speedV * Input.GetAxis("Mouse Y");
+            }
+            else{
+                if(yaw < 0){
+                    yaw = -39.99999f;
+                } else {
+                    yaw = 39.99999f;
+                }
             }
         }
 
-        
-        // transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f); //can change x = pitch to move cam vision up down
+        transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f); //can change x = pitch to move cam vision up down
     }
 }
